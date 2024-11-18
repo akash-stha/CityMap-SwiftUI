62750e47958ac5df1268345c0fe24e8391ac666c
Akash Shrestha <aakashtha49@gmail.com>
2024-11-17 20:28:07 -0500

    Project added

---
+++ CityMap_SwiftUI/CityMap_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityMap_SwiftUIApp.swift
+//  CityMap_SwiftUI
+//
+//  Created by Akash Shrestha on 2024-11-17.
+//
+
+import SwiftUI
+
+@main
+struct CityMap_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
